28a7ed5d015bcf2cde4f3898567e87099bc4b4bb
George Stavrinos <gstavrinos@iit.demokritos.gr>
2017-02-02 13:43:01 +0200

    Experimental version using trigonometry

--- scripts/at.py
+++ scripts/at.py
@@ -1,15 +1,37 @@
 #!/usr/bin/env python
 import rospy
 import tf
+import math
 from sensor_msgs.msg import Imu
 from geometry_msgs.msg import Twist
 
 twist_publisher = None
+obstacle_height = 0.136 #m
+wheel_separation = 0.36 #m
+found_sweet_spot = False
+
+def approx(x, y):
+    return abs(abs(x)-abs(y)) < 0.001
 
 def imu_callback(msg):
-    global pitch_threshold, twist_publisher, cnt
+    global pitch_threshold, twist_publisher, cnt, obstacle_height, wheel_separation, found_sweet_spot
     (roll, pitch, yaw) = tf.transformations.euler_from_quaternion([msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w])
-    print "PITCH = " + str(pitch)
+    #print "PITCH = " + str(pitch)
+    sweet_spot = obstacle_height/wheel_separation;
+    sweet_spot = math.sin(sweet_spot)
+    sweet_spot = math.asin(sweet_spot)
+    #print str(approx(sweet_spot, pitch))
+    if approx(sweet_spot, pitch):
+        found_sweet_spot = True
+        print "SWEET SPOT = " + str(sweet_spot)
+    if found_sweet_spot and abs(pitch) > 0.05:
+        full_force = Twist()
+        full_force.linear.x = 0.6
+        twist_publisher.publish(full_force)
+        print 'FULL FORCE!'
+    elif found_sweet_spot:
+        print 'NOW STOP!'
+        twist_publisher.publish(Twist())
     #twist_publisher.publish(Twist())
 
 def init():
