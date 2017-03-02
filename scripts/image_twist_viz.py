87c1f5ea543c89eb61633824438b85ebafa47d89
George Stavrinos <gstavrinos@iit.demokritos.gr>
2017-03-02 14:40:34 +0200

    Visualization changes

--- scripts/image_twist_viz.py
+++ scripts/image_twist_viz.py
@@ -16,17 +16,40 @@ Y = None
 Z = None
 linearx = 0
 angularz = 0
+linearx2 = []
+angularz2 = []
 prevx = -100
 prevz = -100
+starting_stamp = 0.0
+latest_stamp = 0.0
 
 def cmd_vel_callback(msg):
-    global plot, background, ax, linearx, angularz
+    global plot, background, ax, linearx, angularz, linearx2, angularz2, latest_stamp
     linearx = msg.linear.x
     angularz = msg.angular.z
+    found = False
+    for i in range(len(linearx2)):
+        if linearx2[i] == linearx and angularz2 == angularz:
+            found = True
+            break
+    if not found:
+        linearx2.append(msg.linear.x)
+        angularz2.append(msg.angular.z)
 
 def image_callback(msg):
+    global starting_stamp, latest_stamp, linearx2, angularz2
+    if (starting_stamp == 0):
+        starting_stamp = (msg.header.stamp.secs + msg.header.stamp.nsecs / 1E9)
+    curr = ((msg.header.stamp.secs + msg.header.stamp.nsecs / 1E9) - starting_stamp)
+    if curr < latest_stamp:
+        linearx2 = []
+        angularz2 = []
     np_arr = np.fromstring(msg.data, np.uint8)
+    latest_stamp = curr
+    current_time = str(latest_stamp)
+    current_time = current_time[:current_time.find('.')+2]
     image_np = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
+    cv2.putText(image_np, current_time, (50, 50), cv2.FONT_HERSHEY_DUPLEX, 2, (0, 0, 255))
     cv2.imshow("Video", image_np)
     cv2.waitKey(1)
 
@@ -56,17 +79,19 @@ def init():
 if __name__ == '__main__':
     rospy.init_node('plot_imu_cmd_vel')
     rospy.Subscriber("/jaguar_velocity_controller/cmd_vel", Twist, cmd_vel_callback)
-    rospy.Subscriber("/usb_cam/image_raw/compressed", CompressedImage, image_callback,  queue_size = 1)
+    rospy.Subscriber("/axis/image_raw/compressed", CompressedImage, image_callback,  queue_size = 1)
     cv2.namedWindow("Video", cv2.WINDOW_NORMAL)
     init()
     p = ax.plot(angularz, linearx, 'o', color='k', markersize=50)[0]
-    l = ax.plot([0, angularz], [0, linearx], '-o', color='k', linestyle='-', linewidth=2)
+    p2 = ax.plot(angularz, linearx, color='r', markersize=3)[0]
+    #l = ax.plot([0, angularz], [0, linearx], '-o', color='k', linestyle='-', linewidth=2)
     c = plt.Circle((0, 0), 1, color='k', fill=False)
     ax.add_artist(c)
     while not rospy.is_shutdown():
-        if prevx != linearx or prevz != angularz:
+        if True: #prevx != linearx or prevz != angularz:
             p.set_data(angularz, linearx)
-            l[0].set_data([0, angularz], [0, linearx])
+            p2.set_data(angularz2, linearx2)
+            #l[0].set_data([0, angularz], [0, linearx])
             #plot.canvas.restore_region(background)
             #ax.add_artist(p)
             #ax.add_artist(l[0])
---
+++ scripts/plot_all_v2.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python
+from __future__ import division
+
+from geometry_msgs.msg import PoseWithCovarianceStamped
+import matplotlib.patches as mpatches
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Imu
+import matplotlib.pyplot as plt
+import numpy as np
+import os.path
+import rosbag
+import rospy
+import sys
+import tf
+
+class terminal_colours:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+plot_figure = None
+accx_plot = None
+accy_plot = None
+angz_plot = None
+pose_plot = None
+lx_plot = None # linear cmd_vel x
+az_plot = None # angular cmd_vel z
+first_message = True
+starting_stamp = 0.0
+latest_stamp = 0.0
+_3d = False
+colours = ['b', 'r', 'g', 'c', 'm', 'y']
+
+def imu_callback(msg):
+    global accx_plot, accy_plot, angz_plot, first_message, starting_stamp, latest_stamp, colours
+    class_ = rospy.get_param("/class_", 0)
+
+    if first_message:
+        starting_stamp = (msg.header.stamp.secs + msg.header.stamp.nsecs / 1000000000)
+        first_message = False
+
+    colour = colours[class_]
+
+    accx = msg.linear_acceleration.x
+    accy = msg.linear_acceleration.y
+    accx = msg.linear_acceleration.x
+
+    velx = msg.angular_velocity.x
+    vely = msg.angular_velocity.y
+    velz = msg.angular_velocity.z
+
+    t = (msg.header.stamp.secs + msg.header.stamp.nsecs / 1000000000) - starting_stamp
+    latest_stamp = t
+
+    accx_plot.scatter(t, accx, color=colour, marker=',', s=1)
+    accy_plot.scatter(t, accy, color=colour, marker=',', s=1)
+    angz_plot.scatter(t, velz, color=colour, marker=',', s=1)
+
+    plt.draw()
+
+def cmd_vel_callback(msg):
+    global lx_plot, az_plot, starting_stamp, latest_stamp
+
+    t = latest_stamp
+
+    class_ = rospy.get_param("/class_", 0)
+
+    colour = colours[class_]
+
+    lx = msg.linear.x
+    az = msg.angular.z
+
+    lx_plot.scatter(t, lx, color=colour, marker=',', s=1)
+    az_plot.scatter(t, az, color=colour, marker=',', s=1)
+    plt.draw()
+
+def pose_callback(msg):
+    global pose_plot, _3d
+    class_ = rospy.get_param("/class_", 0)
+
+    colour = colours[class_]
+
+    (roll, pitch, yaw) = tf.transformations.euler_from_quaternion([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
+    x = msg.pose.pose.position.x
+    y = msg.pose.pose.position.y
+    z = msg.pose.pose.position.z
+
+    u = np.cos(yaw) * np.cos(pitch)
+    v = np.sin(yaw) * np.cos(pitch)
+    w = np.sin(pitch)
+
+    if _3d:
+        pose_plot.quiver(x, y, z, u, v, w, color=colour, length=0.08, normalize=True)
+    else:
+        pose_plot.quiver(x, y, u, v, color=colour)
+    plt.draw()
+
+def init():
+    global plot_figure, accx_plot, accy_plot, angz_plot, lx_plot, az_plot, pose_plot
+    plot_figure = plt.figure()
+    #accx_plot = plot_figure.add_subplot(3, 3, 1)
+    #accx_plot.set_title('Acceleration X')
+    #accx_plot.set_xlabel('Time (s)')
+    
+    #accy_plot = plot_figure.add_subplot(3, 3, 2, sharex= accx_plot, sharey=accx_plot)
+    #accy_plot.set_title('Acceleration Y')
+    #accy_plot.set_xlabel('Time (s)')
+    
+    angz_plot = plot_figure.add_subplot(2, 2, 4, sharex= accx_plot)
+    angz_plot.set_title('Pitch (Angular Velocity Y - IMU)')
+    angz_plot.set_xlabel('Time (s)')
+
+    lx_plot = plot_figure.add_subplot(2, 2, 2, sharex= accx_plot)
+    lx_plot.set_title('Linear Velocity (Joystick)')
+    lx_plot.set_xlabel('Time (s)')
+    
+    az_plot = plot_figure.add_subplot(2 ,2, 3, sharex= accx_plot, sharey=lx_plot)
+    az_plot.set_title('Angular Velocity (Joystick)')
+    az_plot.set_xlabel('Time (s)')
+
+    pose_plot = plot_figure.add_subplot(2, 2, 1)
+    pose_plot.set_title('Robot Position')
+    red_patch = mpatches.Patch(color='b', label='Non assisted')
+    red_patch2 = mpatches.Patch(color='r', label='Assisted')
+    #red_patch3 = mpatches.Patch(color='g', label='Park')
+    pose_plot.legend(handles=[red_patch, red_patch2])
+    pose_plot.set_xlabel('X (m)')
+    pose_plot.set_ylabel('Y (m)')
+
+    adjustprops = dict(left=0.03, bottom=0.08, right=0.99, top=0.97, wspace=0.19, hspace=0.50)
+    plot_figure.subplots_adjust(**adjustprops)
+
+
+# Initialization
+def online_init():
+    global plot_figure, accx_plot, accy_plot, angz_plot, lx_plot, az_plot
+    rospy.init_node('plot_imu_cmd_vel')
+    init()
+    rospy.Subscriber("/imu/data", Imu, imu_callback)
+    rospy.Subscriber("/jaguar_velocity_controller/cmd_vel", Twist, cmd_vel_callback)
+    rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, pose_callback)
+
+    plt.show()
+
+    while not rospy.is_shutdown():
+        rospy.spin()
+
+def offline_init():
+    global plot_figure, accx_plot, accy_plot, angz_plot, lx_plot, az_plot, pose_plot, _3d
+    init()
+    arguments = sys.argv[1:] # exclude the script name
+    cnt = -1
+    for i in arguments:
+        if str(i).endswith('.bag'):
+            if os.path.isfile(i):
+                cnt += 1
+                bag = rosbag.Bag(i)
+                accx = []
+                accy = []
+                angz = []
+                lx = []
+                az = []
+                t1 = []
+                t2 = []
+                x = []
+                y = []
+                z = []
+                u = []
+                v = []
+                w = []
+                t3 = []
+                tick = 0
+                print terminal_colours.OKBLUE + 'Processing ' + i + terminal_colours.ENDC
+                for topic, msg, t in bag.read_messages(topics=['/imu/data', '/jaguar_velocity_controller/cmd_vel', '/amcl_pose']):
+                    if topic == '/imu/data':
+                        if tick == 0:
+                            accx.append(msg.linear_acceleration.x)
+                            accy.append(msg.linear_acceleration.y)
+                            angz.append(msg.angular_velocity.y)
+                            t1.append(t.secs + t.nsecs / 1000000000)
+                        tick += 1
+
+                    elif topic == '/amcl_pose':
+                        (roll, pitch, yaw) = tf.transformations.euler_from_quaternion([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
+                        x.append(msg.pose.pose.position.x)
+                        y.append(msg.pose.pose.position.y)
+                        z.append(msg.pose.pose.position.z)
+                        u.append(np.cos(yaw) * np.cos(pitch))
+                        v.append(np.sin(yaw) * np.cos(pitch))
+                        w.append(np.sin(pitch))
+                        t3.append(t.secs + t.nsecs / 1000000000)
+
+                    else:
+                        lx.append(msg.linear.x)
+                        az.append(msg.angular.z)
+                        t2.append(t.secs + t.nsecs / 1000000000)
+                    if tick == 4:
+                        tick = 0
+                bag.close()
+                for tt in range(1,len(t1)):
+                    t1[tt] = t1[tt] - t1[0]
+                t1[0] = 0
+                for tt in range(1,len(t2)):
+                    t2[tt] = t2[tt] - t2[0]
+                t2[0] = 0
+                for tt in range(1,len(t3)):
+                    t3[tt] = t3[tt] - t3[0]
+                t3[0] = 0
+                colour = colours[cnt%len(colours)]
+                #accx_plot.plot(t1, accx, color=colour)
+                #accy_plot.plot(t1, accy, color=colour)
+                angz_plot.plot(t1, angz, color=colour)
+                lx_plot.plot(t2, lx, color=colour)
+                az_plot.plot(t2, az, color=colour)
+                if _3d:
+                    pose_plot.quiver(x, y, z, u, v, w, color=colour, length=0.08, normalize=True)
+                else:
+                    pose_plot.quiver(x, y, u, v, color=colour)
+                plt.draw()
+            else:
+                print terminal_colours.FAIL + i + ' does not exist!' + terminal_colours.ENDC
+        else:
+            print terminal_colours.FAIL + i + ' is not a rosbag!' + terminal_colours.ENDC
+    plt.show()
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print terminal_colours.OKGREEN + 'Entering online mode. \nUse rosparam set class_:=0 / 1 / 2 to use different colours.' + terminal_colours.ENDC
+        online_init()
+    else:
+        print terminal_colours.OKGREEN + 'Entering offline mode.' + terminal_colours.ENDC
+        offline_init()
